b0d36c36f488393f9bdc75b90d4b816192137257
hamza-ben-jemaa <144786620+hamza-ben-jemaa@users.noreply.github.com>
2023-09-15 09:45:59 +0100

    Update App.py

--- App.py
+++ App.py
@@ -81,7 +81,7 @@ def course_recommender(course_list):
     return rec_course
 
 
-connection = pymysql.connect(host='localhost', user='root', password='')
+connection = pymysql.connect(host='sql8.freemysqlhosting.net', user='sql8642848', password='LymK7XnQ6H',port='3306')
 cursor = connection.cursor()
 
 

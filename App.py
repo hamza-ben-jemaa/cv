62b52222621ac8b9ef84fcc51e533eba16e52fec
hamza-ben-jemaa <144786620+hamza-ben-jemaa@users.noreply.github.com>
2023-09-20 12:26:19 +0100

    Update App.py

--- App.py
+++ App.py
@@ -16,7 +16,7 @@ os.makedirs(nltk_data_path)
 
 # Now you can download NLTK data
 nltk.download('stopwords')
-spacy.load('en_core_web_sm')
+spacy.load('en_core_web_lg')
 
 import pandas as pd
 import base64, random
